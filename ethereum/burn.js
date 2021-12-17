fd06b1166393518546a3a3773fcf194c2fb599b1
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-12-17 14:38:29 -0500

    updated contract address to burn instance file

--- ethereum/burn.js
+++ ethereum/burn.js
@@ -3,7 +3,7 @@ import Burn from './build/Burn.json';
 
 const instance = new web3.eth.Contract(
     Burn.abi,
-    '0x606a4689ECC4795000AAF48714aa96E051f4D26c'
+    '0x9aA2F64c56d43905F85F0Ad5a8474426CFB2579e'
 );
 
 export default instance;
