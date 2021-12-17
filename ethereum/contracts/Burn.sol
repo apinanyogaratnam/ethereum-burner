b10904baeba9cde50f108e13403554666f79edcf
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-12-17 14:37:14 -0500

    completed updated contract

--- ethereum/contracts/Burn.sol
+++ ethereum/contracts/Burn.sol
@@ -5,12 +5,14 @@ contract Burn {
     uint totalBurned = 0;
     constructor() {}
 
+    // this will be invalid due to contract being destroyed
     function getTotalBurned() public view returns (uint) {
         return totalBurned;
     }
     
     function burn() public payable {
         totalBurned += msg.value;
-        selfdestruct(payable(address(this)));
+        // selfdestruct(payable(address(this))); // this destroys the contract
+        payable(address(0x0000000000000000000000000000000000000000)).transfer(address(this).balance); // this transfers the contract to the caller
     }
 }
